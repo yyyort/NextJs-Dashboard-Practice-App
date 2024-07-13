54a752046bfa24095fe96a89775f23af5b97ffdc
IanTroy <91863728+yyyort@users.noreply.github.com>
2024-07-14 03:19:34 +0800

    chapter 12, mutating data using server action

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,8 +14,8 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+   /*  console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000)); */
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -30,8 +30,8 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    /* console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000)); */
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
